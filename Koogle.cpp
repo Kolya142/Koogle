1efaa1a1f8f27be4f1ac9460ff4aa7a3cb3fb676
Kolya142 <nikolasshevelkos@gmail.com>
2025-12-06 00:29:53 +0300

    Fix identation.

--- Koogle.cpp
+++ Koogle.cpp
@@ -12,12 +12,12 @@
 
 bool is_linkc(char c) {
     return c == ':' || c == '/' || isalnum(c) ||
-	c == '-' || c == '.' || c == '_' || c == '~' ||
-	c == ':' || c == '/' || c == '?' || c == '#' ||
-	c == '[' || c == ']' || c == '@' || c == '!' ||
-	c == '$' || c == '&' || c == '(' || c == ')' ||
-	c == '*' || c == '+' || c == ',' || c == ';' ||
-	c == '%' || c == '=' || c == '\'';
+        c == '-' || c == '.' || c == '_' || c == '~' ||
+        c == ':' || c == '/' || c == '?' || c == '#' ||
+        c == '[' || c == ']' || c == '@' || c == '!' ||
+        c == '$' || c == '&' || c == '(' || c == ')' ||
+        c == '*' || c == '+' || c == ',' || c == ';' ||
+        c == '%' || c == '=' || c == '\'';
 }
 
 bool is_link_path(std::string s, size_t it) { // TODO
@@ -26,12 +26,12 @@ bool is_link_path(std::string s, size_t it) { // TODO
     size_t last_slesh_it = 0;
     ++it;
     while (it < s_size) {
-	if (s[it] == '/') {
-	    if (last_slesh_it + 1 == it)
-		return false;
-	    last_slesh_it = it;
-	}
-	++it;
+        if (s[it] == '/') {
+            if (last_slesh_it + 1 == it)
+                return false;
+            last_slesh_it = it;
+        }
+        ++it;
     }
     return true;
 }
@@ -42,36 +42,36 @@ bool is_link(std::string s) {
     size_t s_size = s.size();
     if (s_size < 2) return false;
     if (s[0] == '/') {
-	if (s[1] == '/') {
-	    if (!is_link_path(s, 2)) return false;
-	    return true;
-	}
-	else {
-	    if (!is_link_path(s, 1)) return false;
-	    return true;
-	}
+        if (s[1] == '/') {
+            if (!is_link_path(s, 2)) return false;
+            return true;
+        }
+        else {
+            if (!is_link_path(s, 1)) return false;
+            return true;
+        }
     }
     else {
-	if (s_size < 7) return false;
-	if (s[0] != 'h') return false;
-	if (s[1] != 't') return false;
-	if (s[2] != 't') return false;
-	if (s[3] != 'p') return false;
-	if (s[4] == 's') {
-	    if (s_size < 8) return false;
-	    if (s[5] != ':') return false;
-	    if (s[6] != '/') return false;
-	    if (s[7] != '/') return false;
-	    if (!is_link_path(s, 8)) return false;
-	    return true;
-	}
-	else {
-	    if (s[4] != ':') return false;
-	    if (s[5] != '/') return false;
-	    if (s[6] != '/') return false;
-	    if (!is_link_path(s, 7)) return false;
-	    return true;
-	}
+        if (s_size < 7) return false;
+        if (s[0] != 'h') return false;
+        if (s[1] != 't') return false;
+        if (s[2] != 't') return false;
+        if (s[3] != 'p') return false;
+        if (s[4] == 's') {
+            if (s_size < 8) return false;
+            if (s[5] != ':') return false;
+            if (s[6] != '/') return false;
+            if (s[7] != '/') return false;
+            if (!is_link_path(s, 8)) return false;
+            return true;
+        }
+        else {
+            if (s[4] != ':') return false;
+            if (s[5] != '/') return false;
+            if (s[6] != '/') return false;
+            if (!is_link_path(s, 7)) return false;
+            return true;
+        }
     }
     return false;
 }
@@ -81,47 +81,47 @@ std::vector<std::string> links_crawler(std::string doc) {
       /index.html
       //example.com/index.html
       http://example.com/index.html
-     */
+    */
     std::vector<std::string> links;
     size_t it_index = 0;
     size_t doc_size = doc.size();
     while (it_index < doc_size) {
-	char it = doc[it_index];
-	std::cout << it_index << ": " << it << "\n";
-	if (is_linkc(it)) {
-	    size_t start_it_index = it_index;
-	    std::string link;
-	    while (it_index < doc_size && is_linkc(it)) {
-		link += it;
-		++it_index;
-		it = doc[it_index];
-		std::cout << "[LINK] " << it_index << ": " << it << "\n";
-		if (it_index-start_it_index >= 9 && !is_link(link)) {
-		    it_index = start_it_index+1;
-		    goto not_a_link;
-		}
-	    }
-	    if (is_link(link)) {
-		links.push_back(link);
-	    }
-	  not_a_link:
-	}
-	else ++it_index;
+        char it = doc[it_index];
+        std::cout << it_index << ": " << it << "\n";
+        if (is_linkc(it)) {
+            size_t start_it_index = it_index;
+            std::string link;
+            while (it_index < doc_size && is_linkc(it)) {
+                link += it;
+                ++it_index;
+                it = doc[it_index];
+                std::cout << "[LINK] " << it_index << ": " << it << "\n";
+                if (it_index-start_it_index >= 9 && !is_link(link)) {
+                    it_index = start_it_index+1;
+                    goto not_a_link;
+                }
+            }
+            if (is_link(link)) {
+                links.push_back(link);
+            }
+          not_a_link:
+        }
+        else ++it_index;
     }
     return links;
 }
 
 int main() {
     const httplib::Headers headers = {
-	{"User-Agent", "Kooglecrawler/" VER_STR "; https://github.com/Kolya142/Koogle"}
+        {"User-Agent", "Kooglecrawler/" VER_STR "; https://github.com/Kolya142/Koogle"}
     };
     httplib::Client cli("http://localhost:8080");
     cli.enable_server_certificate_verification(true);
 
     if (auto res = cli.Get("/", headers)) {
-	auto links = links_crawler(res->body);
-	for (size_t it = 0; it < links.size(); ++it) {
-	    std::cout << links[it] << '\n';
-	}
+        auto links = links_crawler(res->body);
+        for (size_t it = 0; it < links.size(); ++it) {
+            std::cout << links[it] << '\n';
+        }
     }
 }
